69496462cd6d1fc981adce2b44eb4c8fc40f8543
dilpreet kaur <dilpreet.kukreja@gmail.com>
2019-09-14 11:36:46 -0700

    message

---
+++ script.js
@@ -0,0 +1,68 @@
+
+		let hiddenElements = document.getElementsByClassName("hidden");
+		let fine_dining_section = document.getElementsByClassName("fine-dining-section")[0];
+		//console.log(fine_dining_section);
+		function checkPosition(){
+			//console.log("scroll");image_plate.classList.add('animate-slidein1');
+			//fine_dining_section[0].classList.add('fade-in');
+			for(let i=0;i<hiddenElements.length;i++){
+				console.log(typeof hiddenElements[i].className);
+				let windowHeight = window.innerHeight;
+				console.log(windowHeight);
+				let positionFromTop = hiddenElements[i].getBoundingClientRect().top;
+
+				if(positionFromTop-windowHeight<=0){
+					//if(hiddenElements[i].className.c)
+					if(hiddenElements[i].className.includes("fadingEffect")){
+						hiddenElements[i].classList.add('fade-in');
+						hiddenElements[i].classList.remove('hidden');
+					}
+					else{
+						hiddenElements[i].classList.add('animate-slidein1');
+						hiddenElements[i].classList.remove('hidden');
+					}
+				
+				}
+			}
+		}
+		window.addEventListener("scroll",checkPosition);
+		////////////slide show///////////
+		var slideIndex = 1;
+		showSlides(slideIndex);
+
+		// Next/previous controls
+		function plusSlides(n) {
+		  showSlides(slideIndex += n);
+		}
+
+		// Thumbnail image controls
+		function currentSlide(n) {
+		  showSlides(slideIndex = n);
+		}
+
+		function showSlides(n) {
+		  var i;
+		  var slides = document.getElementsByClassName("mySlides");
+		  var dots = document.getElementsByClassName("dot");
+		  if (n > slides.length) {slideIndex = 1} 
+		  if (n < 1) {slideIndex = slides.length}
+		  for (i = 0; i < slides.length; i++) {
+		      slides[i].style.display = "none"; 
+		  }
+		  for (i = 0; i < dots.length; i++) {
+		      dots[i].classList.remove('active');
+		  }
+		  slides[slideIndex-1].style.display = "block"; 
+		  dots[slideIndex-1].classList.add('active');	  
+		}
+		let icon = document.getElementsByClassName("icon")[0];
+		console.log(icon);
+		function toggleClass(e){
+			let items = document.getElementsByClassName("items");
+			for(let elem of items){
+				elem.classList.toggle('showHide');
+			}
+		}
+		icon.addEventListener("click", toggleClass);
+
+
